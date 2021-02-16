dd1c2051bbebe8d7556a159058e725e96baf4ff2
Somi Gupta <somigupta@sudharshans-MacBook-Pro.local>
2021-02-15 20:58:12 -0500

    Updated code to display box

--- assests/js/script.js
+++ assests/js/script.js
@@ -3,14 +3,17 @@ var timeLabel = document.querySelector("#time");
 var scoreLabel = document.querySelector("#score");
 var questions = document.querySelector(".quiz_ques");
 var quesOptions = document.querySelector("#option_list");
-var infoBox = document.querySelector("#info_box");
+var infoBox = document.querySelector(".info_box");
 
 startButton.addEventListener("click",startQuiz);
 
 function startQuiz(){
     for (var i = 0; i<jsQuestions.length; i++){
-        console.log('Started');
-        // questions.setAttribute("class", inline);
-        questions.remove('hide');
+    infoBox.setAttribute("class", "visibility: hidden");
+    startButton.setAttribute("class", "visibility: hidden");
+        console.log('Started'); 
+        console.log(jsQuestions[i]);
+        questions.textContent = jsQuestions[i];
     }
-}
+}        
+// questions.setAttribute("class", inline);
