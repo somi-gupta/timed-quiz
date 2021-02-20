dfa03df212822a79e777ff7910447da8d3a70e63
Somi Gupta <somigupta@sudharshans-MacBook-Pro.local>
2021-02-20 01:28:49 -0500

    Updated and added score function and return button

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,13 +1,16 @@
+// Global variables
 var startButton = document.querySelector(".startbtn");
 var timeLabel = document.querySelector("#time");
 var scoreLabel = document.querySelector("#score");
 var questions = document.querySelector(".quiz_ques");
 var infoBox = document.querySelector(".info_box");
 let choiceBox = document.createElement("ul");
-var answerBlock  = document.querySelector("#showAnswer");
+var answerBlock = document.querySelector("#showAnswer");
+var pTags = document.querySelectorAll("p");
+var inputBox = document.createElement("input");
 var questionIndex = 0;
 var questionsCorrect = 0;
-var timeleft = jsQuestions.length * 10;
+var timeleft;
 console.log(timeleft);
 var timerInterval;
 
@@ -18,6 +21,7 @@ startButton.addEventListener("click", function () {
 
 //Function to set timer
 function setTime() {
+  timeleft = jsQuestions.length * 10;
   questionIndex = 0;
   timerInterval = setInterval(function () {
     timeleft--;
@@ -33,9 +37,7 @@ function setTime() {
 
 //Function to render questions and answer options.
 function startQuiz(questionIndex) {
-    infoBox.innerHTML = "";
-    startButton.setAttribute("style", "visibility: hidden");
-
+  infoBox.innerHTML = "";
   choiceBox.innerHTML = "";
 
   var questionArray = jsQuestions[questionIndex].title;
@@ -49,50 +51,137 @@ function startQuiz(questionIndex) {
     choiceBox.appendChild(listChoice);
 
     listChoice.addEventListener("click", userAnswer);
-
-  })
-
+  });
 }
 
 function userAnswer(event) {
   var element = event.target;
 
   if (element.matches("li")) {
+    if (element.textContent === jsQuestions[questionIndex].answer) {
+      answerBlock.textContent = "Correct!";
 
-  if (element.textContent === jsQuestions[questionIndex].answer) {
-    // alert("Correct answer");
-    answerBlock.textContent = "Correct!";
-  } else {
-    timeleft = timeleft - 10;
-    answerBlock.textContent = "Incorrect!";
+    } else {
+      timeleft = timeleft - 10;
+      answerBlock.textContent = "Incorrect!";
+    }
   }
-}
-
   questionIndex++;
   if (questionIndex >= jsQuestions.length) {
-      console.log("jsQuestions: " ,questionIndex >= jsQuestions.length);
-      console.log(questionIndex);
     score();
   } else {
     startQuiz(questionIndex);
   }
 }
 
-function score(){
-    questions.innerHTML = "";
-    timeLabel.innerHTML = "";
-    answerBlock.innerHTML = "";
+function score() {
+  questions.innerHTML = "";
+  timeLabel.innerHTML = "";
+  answerBlock.innerHTML = "";
 
-    if(timeleft >= 0){
-        var userScore = timeleft;
-        clearInterval(timerInterval);
-        var message = "All Done!. \n Your final score is: " ;
-        infoBox.textContent = message + userScore ;
+  if (timeleft >= 0) {
+    var userScore = timeleft;
+    clearInterval(timerInterval);
 
-        var label = document.createElement("label");
-        label.htmlFor = "Enter Initials"
-        var input = document.createElement("input");
-        input.type = "text";
-    }
+    var heading = document.createElement("h1");
+    heading.innerHTML = "All Done!.";
+    infoBox.append(heading);
 
+    var para = document.createElement("p");
+    para.innerHTML = "Your score is: " + userScore;
+    infoBox.append(para);
+
+    var label = document.createElement("label");
+    label.innerHTML = "Enter Initials: ";
+    infoBox.append(label);
+
+    inputBox.type = "text";
+    label.append(inputBox);
+
+    var submitInitials = document.createElement("button");
+    submitInitials.innerHTML = " Submit";
+    label.appendChild(submitInitials);
+
+    submitInitials.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      if (inputBox.value == "") {
+        var errorMsg = document.createElement("p");
+        errorMsg.innerHTML = "Please enter initials";
+        label.append(errorMsg);
+      } else {
+        var inputInit = inputBox.value + " - " + timeleft;
+
+        initialsArr.push(inputInit);
+
+        localStorage.setItem("userInitials", JSON.stringify(initialsArr));
+        inputBox.value = "";
+        renderhighScore();
+      }
+    });
+  }
 }
+var initialsArr = [];
+var returnButton = document.createElement("button");
+returnButton.setAttribute("id", "return-button");
+var clearHighScore = document.createElement("button");
+var initialList = document.createElement("ul");
+
+function renderhighScore() {
+  infoBox.innerHTML = "";
+  var storedInitials = JSON.parse(localStorage.getItem("userInitials"));
+
+  initialsArr = storedInitials;
+  console.log(initialsArr);
+
+  var highScore = document.createElement("p");
+  highScore.textContent = "High Score";
+  infoBox.append(highScore);
+
+  highScore.append(initialList);
+
+  for (var i = 0; i < initialsArr.length; i++) {
+    var init = initialsArr[i];
+    var listChoice = document.createElement("li");
+
+    listChoice.textContent = init;
+    listChoice.setAttribute("data-index", i);
+
+  }
+  initialList.appendChild(listChoice);
+
+  returnButton.textContent = "Return";
+    returnButton.style.marginRight = "20px";
+    returnButton.style.fontSize = "20px";
+
+    clearHighScore.textContent = "Clear HighScore";
+    clearHighScore.style.fontSize = "20px";
+
+    highScore.append(returnButton);
+    highScore.append(clearHighScore);
+  returnButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    setInfo();
+  });
+}
+
+function setInfo() {
+  infoBox.innerHTML = "";
+
+  var heading = document.createElement("h1");
+  heading.innerHTML = "Coding Quiz Challenge";
+
+  var para = document.createElement("p");
+  para.innerHTML =
+    "Try to answer the following code-related questions within the time limit." +
+    "Keep in mind that incorrect answers will penalize your score time by 10 seconds.";
+  infoBox.append(heading);
+  infoBox.append(para);
+  infoBox.append(startButton);
+}
+
+setInfo();
+
+clearHighScore.addEventListener("click", function () {
+  initialList.innerHTML = "";
+});
